a1f14fcc9bdf5ab19dd1c984902996e1dcac13bb
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-10-20 23:56:38 +0800

    update footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -7,21 +7,20 @@ const Footer = () => {
                 <div className="mt-16 basis-1/2 md:mt-0">
                     <img alt="logo" src={Logo} />
                     <p className="my-5">
-                        Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
-                        purus et arcu massa dictum condimentum. Augue scelerisque iaculis
-                        orci ut habitant laoreet. Iaculis tristique.
+                        Transforming the way you think about fitness. Dive into a world where workouts become a lifestyle,
+                        and every challenge pushes you to evolve. We're not just a gym - we're a movement.
                     </p>
                     <p>© Evogym All Rights Reserved.</p>
                 </div>
                 <div className="mt-16 basis-1/4 md:mt-0">
                     <h4 className="font-bold">Links</h4>
-                    <p className="my-5">Massa orci senectus</p>
-                    <p className="my-5">Et gravida id et etiam</p>
-                    <p>Ullamcorper vivamus</p>
+                    <p className="my-5">Home</p>
+                    <p className="my-5">Classes</p>
+                    <p>Membership</p>
                 </div>
                 <div className="mt-16 basis-1/4 md:mt-0">
                     <h4 className="font-bold">Contact Us</h4>
-                    <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+                    <p className="my-5">Trainers</p>
                     <p>(333)425-6825</p>
                 </div>
             </div>
