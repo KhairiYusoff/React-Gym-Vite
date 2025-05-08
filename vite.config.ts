4b96a4ccd792a4e53ef550936bceaf10efd6a558
Mohd Khairi Mohd Yusoff <mohdkhairi.mohdyusoff@maybank.com>
2025-05-09 00:41:52 +0800

    create deploy.yml file

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/React-Gym-Vite",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-  preview: {
+  server: {
     host: true,
-    port: 8080,
+    port: 3001,
   },
 });
