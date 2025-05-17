d8f5bd060eb7ca2b4afa58249081c0a586652ad6
Mohd Khairi Mohd Yusoff <mohdkhairi.mohdyusoff@maybank.com>
2025-05-18 00:01:47 +0800

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Gym-Vite",
+  base: process.env.GITHUB_PAGES ? "/React-Gym-Vite/" : "/", 
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
