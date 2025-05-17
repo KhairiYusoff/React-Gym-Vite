f461d6e7fd244b974dd079a2c320169864713d02
Mohd Khairi Mohd Yusoff <mohdkhairi.mohdyusoff@maybank.com>
2025-05-18 00:10:28 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? "/React-Gym-Vite/" : "/", 
+  base: isGithubPages ? '/React-Gym-Vite/' : '/', 
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   server: {
     host: true,
